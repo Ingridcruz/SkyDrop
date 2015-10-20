6d84c23e55e7fb8900039843a7368d6f2f2248db
Ingridcruz <ing_br_09@hotmail.com>
2015-10-20 00:49:04 -0500

    enemigos
    Los enemigos bajan aleatoriamente y cuando chocan con el personaje
    desaparecen los enemigos

--- app/src/main/java/mx/itesm/skydrop/Enemigo.java
+++ app/src/main/java/mx/itesm/skydrop/Enemigo.java
@@ -1,28 +1,31 @@
 package mx.itesm.skydrop;
 
+import org.andengine.entity.IEntity;
 import org.andengine.entity.sprite.Sprite;
 
 /**
  * Created by Ingrid on 12/10/2015.
  */
-public class Enemigo extends EscenaNivel1{
+public class Enemigo {
 
-        private Sprite spriteEnemigo;
+    private Sprite spriteEnemigo;
 
-        public Enemigo(Sprite sprite) {
-            spriteEnemigo = sprite;
-        }
-
-        public Sprite getSpriteEnemigo() {
-            return spriteEnemigo;
-        }
-
-        public void setSpriteEnemigo(Sprite spriteEnemigo) {
-            this.spriteEnemigo = spriteEnemigo;
-        }
-
-        public void mover(int dx, int dy) {
-            spriteEnemigo.setPosition( spriteEnemigo.getX()+dx, spriteEnemigo.getY()+dy );
-        }
+    public Enemigo(Sprite sprite) {
+        this.spriteEnemigo = sprite;
     }
+
+    public Sprite getSpriteEnemigo() {
+        return spriteEnemigo;
+    }
+
+    public void setSpriteEnemigo(Sprite spriteEnemigo) {
+        this.spriteEnemigo = spriteEnemigo;
+    }
+
+    public void mover(int dx, int dy) {
+        spriteEnemigo.setPosition(spriteEnemigo.getX() + dx, spriteEnemigo.getY() + dy);
+    }
+}
+
+  
 
--- app/src/main/java/mx/itesm/skydrop/EscenaNivel1.java
+++ app/src/main/java/mx/itesm/skydrop/EscenaNivel1.java
@@ -1,18 +1,28 @@
 package mx.itesm.skydrop;
 
+import android.util.Log;
+
 import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.input.sensor.acceleration.AccelerationData;
 import org.andengine.input.sensor.acceleration.IAccelerationListener;
+import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.texture.region.TiledTextureRegion;
+
+import java.util.ArrayList;
 
 /**
  * Created by Ingrid on 01/10/2015.
  */
 public class EscenaNivel1 extends EscenaBase implements IAccelerationListener {
 
+    private int energia = 100;
 
+    private boolean juegoCorriendo = true;
+
+    // Fin del juego
+    private ITextureRegion regionFin;
         private ITextureRegion regionFondo;
         private ITextureRegion regionFondo2;
     private AnimatedSprite spritePersonaje;
@@ -20,12 +30,19 @@ public class EscenaNivel1 extends EscenaBase implements IAccelerationListener {
         // Sprite para el fondo
         private Sprite spriteFondo;
         private Sprite spriteFondo2;
+    private ArrayList<Enemigo> listaEnemigos;
+    private ITextureRegion regionEnemigo;
+    // Tiempo para generar listaEnemigos
+    private float tiempoEnemigos = 0;
+    private float LIMITE_TIEMPO = 2.5f;
+
 
         @Override
         public void cargarRecursos() {
             regionFondo = cargarImagen("escenario.jpg");
             regionFondo2 = cargarImagen("escenario2.jpg");
             regionPersonajeAnimado = cargarImagenMosaico("pajaro.jpg", 128, 29, 1, 3);
+            regionEnemigo=cargarImagen("nube.png");
         }
 
 
@@ -40,17 +57,72 @@ public class EscenaNivel1 extends EscenaBase implements IAccelerationListener {
         spritePersonaje.animate(150);   // 200ms entre frames, 1000/200 fps
         attachChild(spritePersonaje);
         actividadJuego.getEngine().enableAccelerationSensor(actividadJuego, this);
+
+        // Lista de enemigos que aparecen del lado derecho
+        listaEnemigos = new ArrayList<>();
     }
+
+
+
         @Override
         protected void onManagedUpdate(float pSecondsElapsed) {
             super.onManagedUpdate(pSecondsElapsed);
             spriteFondo.setY(spriteFondo.getY() - 5);
-            spriteFondo2.setY(spriteFondo2.getY()-5);
+            spriteFondo2.setY(spriteFondo2.getY() - 5);
             if (spriteFondo.getY()<-spriteFondo.getHeight()/2){
                 spriteFondo.setY(1.5f*spriteFondo.getHeight()-5);
             }
             if (spriteFondo2.getY()<-spriteFondo2.getHeight()/2){
                 spriteFondo2.setY(1.5f*spriteFondo2.getHeight()-5);
+            }
+            //manejo enemigos
+            if (!juegoCorriendo) {
+                return;
+            }
+            // Acumular tiempo
+            tiempoEnemigos += pSecondsElapsed;
+            if (tiempoEnemigos > LIMITE_TIEMPO) {
+                // Se cumplió el tiempo
+                tiempoEnemigos = 0;
+                if (LIMITE_TIEMPO > 0.5f) {
+                    LIMITE_TIEMPO -= 0.15f;
+                }
+                Sprite spriteEnemigo = cargarSprite((float) (Math.random() * ControlJuego.ANCHO_CAMARA - regionEnemigo.getWidth()) + regionEnemigo.getWidth(),ControlJuego.ALTO_CAMARA + regionEnemigo.getHeight(), regionEnemigo);
+                Enemigo nuevoEnemigo = new Enemigo(spriteEnemigo);
+                listaEnemigos.add(nuevoEnemigo);
+                attachChild(nuevoEnemigo.getSpriteEnemigo());
+                Log.i("Tamaño", "Datos: " + listaEnemigos.size());
+            }
+            // Actualizar cada uno de los listaEnemigos y ver si alguno ya salió de la pantalla
+            for (int i = listaEnemigos.size() - 1; i >= 0; i--) {
+                Enemigo enemigo = listaEnemigos.get(i);
+                enemigo.mover(0, -10);
+                if (enemigo.getSpriteEnemigo().getY() < -enemigo.getSpriteEnemigo().getHeight()) {
+                    detachChild(enemigo.getSpriteEnemigo());
+                    listaEnemigos.remove(enemigo);
+                }
+                if (spritePersonaje.collidesWith(enemigo.getSpriteEnemigo())) {
+                    detachChild(enemigo.getSpriteEnemigo());
+                    listaEnemigos.remove(enemigo);
+                    energia = 10;
+                    Log.i("ENERGIA", "Energia: " + energia);
+                    if (energia <= 0) {
+                        juegoCorriendo = false;
+                        // Agrega pantalla de fin
+                        Sprite spriteFin = new Sprite(ControlJuego.ANCHO_CAMARA / 2, ControlJuego.ALTO_CAMARA / 2,
+                                regionFin, actividadJuego.getVertexBufferObjectManager()) {
+                            @Override
+                            public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY) {
+                                if (pSceneTouchEvent.isActionUp()) {
+                                    onBackKeyPressed();
+                                }
+                                return super.onAreaTouched(pSceneTouchEvent, pTouchAreaLocalX, pTouchAreaLocalY);
+                            }
+                        };
+                        registerTouchArea(spriteFin);
+                        attachChild(spriteFin);
+                    };
+                }
             }
         }
         @Override
@@ -75,7 +147,10 @@ public class EscenaNivel1 extends EscenaBase implements IAccelerationListener {
             regionFondo = null;
         }
 
-    @Override
+
+
+
+            @Override
     public void onAccelerationAccuracyChanged(AccelerationData pAccelerationData) {
 
     }
@@ -88,14 +163,23 @@ public class EscenaNivel1 extends EscenaBase implements IAccelerationListener {
         float dy = 0;
         //desplazamiento en y
 
-        float dz = pAccelerationData.getZ();
-        //desplazamiento en z
-        spritePersonaje.setX(spritePersonaje.getX()+dx);
-        spritePersonaje.setY(spritePersonaje.getY()+ dy);
+        if(dx>=0) {
+            if(spritePersonaje.getX()<=800) {
+                spritePersonaje.setX(spritePersonaje.getX() + dx);
+            }
+        }
+        if(dx<=0) {
+            if(spritePersonaje.getX()>=0) {
+                spritePersonaje.setX(spritePersonaje.getX() + dx);
+            }
+        }
+
+
 
 
 
     }
+
 
 
     public void liberarRecurso () {
@@ -106,6 +190,10 @@ public class EscenaNivel1 extends EscenaBase implements IAccelerationListener {
         regionFondo = null;
         regionPersonajeAnimado.getTexture().unload();
         regionPersonajeAnimado = null;
+        regionEnemigo.getTexture().unload();
+        regionEnemigo = null;
+        regionFin.getTexture().unload();
+        regionFin = null;
 
     }
 
